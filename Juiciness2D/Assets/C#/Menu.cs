048e4218adad180aa458c5650d076b2f99afb700
Daniel-something <79511435+Daniel-something@users.noreply.github.com>
2021-10-12 11:33:37 +0200

    Menu Updatering
    Koden borde fungera efter att man lagt in båda scenerna i build settings.

--- Juiciness2D/Assets/C#/Menu.cs
+++ Juiciness2D/Assets/C#/Menu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
@@ -13,6 +14,19 @@ public class Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("SampleScene"); //Laddar in scenen "SampleScene" om man trycker på Space i menyn. - Daniel
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Application.Quit(); //Stänger ner programmet om man trycker på shift i menyn. - Daniel
+        }
     }
 }
