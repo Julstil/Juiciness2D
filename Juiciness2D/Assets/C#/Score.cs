5d4ffef7505b0ad40bb2d4f52b163b2cf08ec3a1
Daniel-something <79511435+Daniel-something@users.noreply.github.com>
2021-10-21 15:24:25 +0200

    Saving highscores
    En uppdatering till Score.cs som lägger till funktionen att den automatiskt sparar highscores samt hämtar det aktuella highscoret när spelet startas.

--- Juiciness2D/Assets/C#/Score.cs
+++ Juiciness2D/Assets/C#/Score.cs
@@ -6,16 +6,22 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public static int score = 0;
+    public static int highScore = 0;
     public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        highScore = PlayerPrefs.GetInt("highscore", 0); //Kollar om det finns ett highscore sparat på datorn ovh tar det värdet. Om det inte finns något värde sätts den till 0. - Daniel
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (score > highScore)
+        { 
+            highScore = score; //Gör att highScore får scores värde om score har ett högre värde. - Daniel
+            PlayerPrefs.SetInt("highscore", score); //Sparar scores värde när score som highScore när den är större än highScore. - Daniel
+        }
         scoreText.text = "Score:" + score; //Skriver ut "Score:" och score variabeln, på så sätt ser man hur mycket poäng man har. - Daniel
     }
 }
