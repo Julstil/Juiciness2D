adf52e9d38ffe62b3816ab6fa96de523759ac20c
Julstil <edvinnordangard@hotmail.com>
2021-10-21 14:39:40 +0200

    Enemy & explotion

---
+++ Juiciness2D/Assets/C#/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    int health;
+    public int startHealth;
+    public static int allPows = 0;
+
+    public GameObject[] Pows = new GameObject[allPows];
+    GameObject POW;
+
+    private void Start()
+    {
+        health = startHealth;
+        int index = Random.Range(0, Pows.Length);
+        POW = Pows[index];
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Update is called once per frame
+    void Die()
+    {
+        Instantiate(POW, transform.position, Quaternion.Euler(0, 0, Random.Range(25, -25)));
+        Destroy(gameObject);
+
+        //Destroy(gameObject, 1); - Edvin
+    }
+}
--- Juiciness2D/Assets/C#/goForward.cs
+++ Juiciness2D/Assets/C#/goForward.cs
@@ -7,6 +7,7 @@ public class goForward : MonoBehaviour
     public Rigidbody2D rb2d;
     public float speed = 20;
     public Vector3 direction;
+    public int bulletDamage = 25;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +17,13 @@ public class goForward : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D hitInfo)
     {
-     
-       print(hitInfo.name);
-       Destroy(gameObject);
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(bulletDamage);
+        }
+
+        Destroy(gameObject);
      
     }
 
