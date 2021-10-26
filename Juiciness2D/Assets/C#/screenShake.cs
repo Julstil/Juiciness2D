96c410022f647479632902fbdc92703d85d256f6
Daniel-something <79511435+Daniel-something@users.noreply.github.com>
2021-10-26 11:22:18 +0200

    Början på Screenshake samt unity fix
    Unity fix: Jag fixade att musiken inte spelade dubbelt, borde fungera och låta som det ska nu.

---
+++ Juiciness2D/Assets/C#/screenShake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenShake : MonoBehaviour
+{
+    public Vector2 frequency; //Kraften av skakningen. - Daniel
+    public Vector2 amplitude; //Hur mycket det ska skakas. - Daniel
+    Vector2 time = Vector2.zero; //Hur mycket tid som har gått. - Daniel
+ static bool shouldShake; //En bool för ifall skärmen ska skaka eller inte. - Daniel
+ static float shakeTime; //Hur länge det ska skaka. - Daniel
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+     
+    // Update is called once per frame
+    void Update()
+    {
+
+        //time = Time.fixeddeltaTime
+        //gameobject.pos
+        //shakepos.x = Sin(rid) * amplitud.x
+        //lokalpos = shakepos
+    }
+}
