4541edff85157136e9c8682260f4e9270c7fc74a
Daniel-something <79511435+Daniel-something@users.noreply.github.com>
2021-10-29 13:50:33 +0200

    Title screen
    Title screen bakgrund med animationer är klara.

--- Juiciness2D/Assets/C#/screenShake.cs
+++ Juiciness2D/Assets/C#/screenShake.cs
@@ -22,16 +22,16 @@ public class screenShake : MonoBehaviour
         if (shakeTime > 0)
         {
             Vector3 localPos = transform.position; //localPos blir kamerans position. - Daniel
-            time.x += Time.time * frequency.x;
-            time.y += Time.time * frequency.y;
+            time.x += Time.time * frequency.x; // Bestämmer hur snabbt kameran ska åka fram och tillbaka i x-ledet när den skakar. - Daniel
+            time.y += Time.time * frequency.y; // Bestämmer hur snabbt kameran ska åka fram och tillbaka i y-ledet när den skakar. - Daniel
             localPos.x = Mathf.Sin(time.x) * amplitude.x;
 
             transform.position = localPos;
-            shakeTime -= Time.deltaTime;
+            shakeTime -= Time.deltaTime; //Minskar shakeTime med deltaTime tills shakeTime är lika med eller mindre än 0, vilket får skärmen att sluta skaka. - Daniel
         }
         else
         {
-            transform.position = startPos;
+            transform.position = startPos; //Om skärmen inte skakar så går kameran tillbaka till startPos, där den är placerad i scenen. - Daniel
         }
     }
 }
